d0848b7e7a66f4ead1c7aea7006c3211ad9f4f62
Sam Doshi <sam@metal-fish.co.uk>
2017-02-15 16:17:13 +0000

    modify OneBig to work with widescreen monitors

--- src/OneBig.hs
+++ src/OneBig.hs
@@ -5,7 +5,8 @@ module OneBig ( OneBig (..)
               ) where
 
 import           Graphics.X11.Xlib.Types (Dimension, Position,
-                                          Rectangle (Rectangle))
+                                          Rectangle (Rectangle), rect_height,
+                                          rect_width)
 import           XMonad.Core             (LayoutClass (pureLayout, pureMessage),
                                           SomeMessage, fromMessage)
 import           XMonad.Layout           (Resize (Expand, Shrink))
@@ -19,66 +20,68 @@ instance LayoutClass OneBig a where
   pureMessage = oneBigMessage
 
 -- | Processes Shrink/Expand messages
--- TODO needs to not set invalid sizes
 oneBigMessage :: OneBig a -> SomeMessage -> Maybe (OneBig a)
 oneBigMessage (OneBig cx cy) m = fmap resize (fromMessage m)
-  where resize Shrink = OneBig (cx-delta) (cy-delta)
-        resize Expand = OneBig (cx+delta) (cy+delta)
-        delta = 2/100
+  where resize Shrink = limitResize (cx - delta) (cy - delta)
+        resize Expand = limitResize (cx + delta) (cy + delta)
+        delta = 1/100
+        limitResize x y | x <= 0 || y <= 0 = OneBig cx cy
+                        | x >= 1 || y >= 1 = OneBig cx cy
+                        | otherwise        = OneBig  x  y
 
 -- | Main layout function
 oneBigLayout :: OneBig a -> Rectangle -> W.Stack a -> [(a, Rectangle)]
-oneBigLayout (OneBig cx cy) rect stack = [(master,masterRect)]
-                                         ++ divideBottom bottomRect bottomWs
-                                         ++ divideRight rightRect rightWs
-  where ws = W.integrate stack
-        n = length ws
-        ht (Rectangle _ _ _ hh) = hh
-        wd (Rectangle _ _ ww _) = ww
-        h' = round (fromIntegral (ht rect)*cy)
-        w = wd rect
-        m = calcBottomWs n w h'
-        master = head ws
-        other  = tail ws
-        bottomWs = take m other
-        rightWs = drop m other
-        masterRect = cmaster n m cx cy rect
-        bottomRect = cbottom cy rect
+oneBigLayout (OneBig cx cy) rect stack = [(masterWindow, masterRect)]
+                                         ++ divideRight rightRect rightWindows
+                                         ++ divideBottom bottomRect bottomWindows
+  where windowStack = W.integrate stack
+        numWindows = length windowStack
+        masterWidth = round $ fromIntegral (rect_width rect) * cx
+        windowHeight = rect_height rect
+        numRightWindows = calcRightWindows numWindows masterWidth windowHeight
+        masterWindow = head windowStack
+        otherWindows  = tail windowStack
+        rightWindows = take numRightWindows otherWindows
+        bottomWindows = drop numRightWindows otherWindows
+        masterRect = cmaster numWindows numRightWindows cx cy rect
+        bottomRect = cbottom cx cy rect
         rightRect  = cright cx cy rect
 
 -- | Calculate how many windows must be placed at bottom
-calcBottomWs :: Int -> Dimension -> Dimension -> Int
-calcBottomWs n w h' = case n of
-                        1 -> 0
-                        2 -> 1
-                        3 -> 1
-                        4 -> 2
-                        _ -> fromIntegral w*(n-1) `div` fromIntegral (h'+fromIntegral w)
+calcRightWindows :: Int        -- ^ Total no of windows
+                 -> Dimension  -- ^ Width of bottom window
+                 -> Dimension  -- ^ Height of bottom window
+                 -> Int        -- ^ No of windows at the bottom
+calcRightWindows 1 _ _ = 0
+calcRightWindows 2 _ _ = 1
+calcRightWindows 3 _ _ = 1
+calcRightWindows 4 _ _ = 2
+calcRightWindows n w h = fromIntegral w*(n-1) `div` fromIntegral (h+fromIntegral w)
 
 -- | Calculate rectangle for master window
 cmaster :: Int -> Int -> Rational -> Rational -> Rectangle -> Rectangle
 cmaster n m cx cy (Rectangle x y sw sh) = Rectangle x y w h
-  where w = if n > m+1 then
-              round (fromIntegral sw*cx)
+  where w = if n > 1 then
+              round (fromIntegral sw * cx)
             else
               sw
-        h = if n > 1 then
-              round (fromIntegral sh*cy)
+        h = if n > m + 1 then
+              round (fromIntegral sh * cy)
             else
               sh
 
 -- | Calculate rectangle for bottom windows
-cbottom :: Rational -> Rectangle -> Rectangle
-cbottom cy (Rectangle sx sy sw sh) = Rectangle sx y sw h
-  where h = round (fromIntegral sh*(1-cy))
-        y = round (fromIntegral sh*cy+fromIntegral sy)
+cbottom :: Rational -> Rational -> Rectangle -> Rectangle
+cbottom cx cy (Rectangle sx sy sw sh) = Rectangle sx y w h
+  where w = round (fromIntegral sw * cx)
+        h = round (fromIntegral sh * (1 - cy))
+        y = round (fromIntegral sh * cy + fromIntegral sy)
 
 -- | Calculate rectangle for right windows
 cright :: Rational -> Rational -> Rectangle -> Rectangle
-cright cx cy (Rectangle sx sy sw sh) = Rectangle x sy w h
-  where w = round (fromIntegral sw*(1-cx))
-        x = round (fromIntegral sw*cx+fromIntegral sx)
-        h = round (fromIntegral sh*cy)
+cright cx _ (Rectangle sx sy sw sh) = Rectangle x sy w sh
+  where w = round (fromIntegral sw * (1 - cx))
+        x = round (fromIntegral sw * cx + fromIntegral sx)
 
 -- | Divide bottom rectangle between windows
 divideBottom :: Rectangle -> [a] -> [(a, Rectangle)]
@@ -90,7 +93,7 @@ divideBottom (Rectangle x y w h) ws = zip ws rects
 
 -- | Divide right rectangle between windows
 divideRight :: Rectangle -> [a] -> [(a, Rectangle)]
-divideRight (Rectangle x y w h) ws = if n==0 then [] else zip ws rects
+divideRight (Rectangle x y w h) ws = if n == 0 then [] else zip ws rects
   where n = length ws
         oneH = fromIntegral h `div` n
         oneRect = Rectangle x y w (fromIntegral oneH)
@@ -98,10 +101,8 @@ divideRight (Rectangle x y w h) ws = if n==0 then [] else zip ws rects
 
 -- | Shift rectangle right
 shiftR :: Position -> Rectangle -> Rectangle
-shiftR s (Rectangle x y w h) = Rectangle (x+s) y w h
+shiftR s (Rectangle x y w h) = Rectangle (x + s) y w h
 
 -- | Shift rectangle bottom
 shiftB :: Position -> Rectangle -> Rectangle
-shiftB s (Rectangle x y w h) = Rectangle x (y+s) w h
-
-
+shiftB s (Rectangle x y w h) = Rectangle x (y + s) w h
