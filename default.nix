bfd4c5d2944172127014dc22e5d1e1704f16245a
Sam Doshi <sam@metal-fish.co.uk>
2019-09-21 11:09:44 +0100

    temporary dbus patch

--- default.nix
+++ default.nix
@@ -1,18 +1,20 @@
 { pkgs ? import <nixpkgs> {} }:
 
-# https://freux.fr/orgposts/env.html
+# https://freux.fr/posts/env.html
 
 let
   hp = pkgs.haskellPackages;  # change this to use a different compiler
   c2n = name: fp: attrs: hp.callCabal2nix name fp attrs;
 in
   rec {
+    # dbus = c2n "dbus" ./vendor/dbus {};
+    dbus = pkgs.haskell.lib.appendPatch hp.dbus ./dbus.patch;
     xmonad = c2n "xmonad" ./vendor/xmonad {};
     xmonad-contrib = c2n "xmonad-contrib" ./vendor/xmonad-contrib { inherit xmonad; };
-    xmonad-samdoshi = c2n "xmonad-samdoshi" ./. { inherit xmonad xmonad-contrib; };
+    xmonad-samdoshi = c2n "xmonad-samdoshi" ./. { inherit dbus xmonad xmonad-contrib; };
     env = hp.shellFor {
       name = "xmonad-samdoshi-env";
       packages = p: [ xmonad-samdoshi ];
-      buildInputs = [ pkgs.cabal-install ];
+      buildInputs = with pkgs;  [ cabal-install stylish-haskell ];
     };
   }
