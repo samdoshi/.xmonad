6cf37cbdd5ebaa2710897ce2f1256e2885044aa0
Sam Doshi <sam@metal-fish.co.uk>
2019-09-28 08:03:03 +0100

    remove .envrc from repo, gitignore in default.nix

--- default.nix
+++ default.nix
@@ -5,12 +5,13 @@
 let
   hp = pkgs.haskellPackages;  # change this to use a different compiler
   c2n = name: fp: attrs: hp.callCabal2nix name fp attrs;
+  fsrc = src: pkgs.nix-gitignore.gitignoreSource [] src;
 in
   rec {
     dbus = pkgs.haskell.lib.appendPatch hp.dbus ./dbus.patch;
-    xmonad = c2n "xmonad" ./vendor/xmonad {};
-    xmonad-contrib = c2n "xmonad-contrib" ./vendor/xmonad-contrib { inherit xmonad; };
-    xmonad-samdoshi = c2n "xmonad-samdoshi" ./. { inherit dbus xmonad xmonad-contrib; };
+    xmonad = c2n "xmonad" (fsrc ./vendor/xmonad) {};
+    xmonad-contrib = c2n "xmonad-contrib" (fsrc ./vendor/xmonad-contrib) { inherit xmonad; };
+    xmonad-samdoshi = c2n "xmonad-samdoshi" (fsrc ./.) { inherit dbus xmonad xmonad-contrib; };
     env = hp.shellFor {
       name = "xmonad-samdoshi-env";
       withHoogle = true;
