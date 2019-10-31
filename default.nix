313acbf040211f0318738b84c6d4c0aab34f8848
Sam Doshi <sam@metal-fish.co.uk>
2019-10-31 18:25:52 +0000

    unpin pkgs

--- default.nix
+++ default.nix
@@ -3,12 +3,12 @@
 # https://freux.fr/posts/env.html
 
 let
-  pinned = import (builtins.fetchTarball {
-    name = "nixos-pinned-xmonad";
-    url = "https://github.com/nixos/nixpkgs/archive/f0fec244ca380b9d3e617ee7b419c59758c8b0f1.tar.gz";
-    sha256 = "0ga51457fb30b8j9v8is7wwf9ld9p51nizm8yhj09l23qpyh8np9";
-  }) {};
-  # pinned = pkgs;
+  #pinned = import (builtins.fetchTarball {
+  #  name = "nixos-pinned-xmonad";
+  #  url = "https://github.com/nixos/nixpkgs/archive/f0fec244ca380b9d3e617ee7b419c59758c8b0f1.tar.gz";
+  #  sha256 = "0ga51457fb30b8j9v8is7wwf9ld9p51nizm8yhj09l23qpyh8np9";
+  #}) {};
+  pinned = pkgs;
   hp = pinned.haskellPackages;  # change this to use a different compiler
   c2n = name: fp: attrs: hp.callCabal2nix name fp attrs;
   fsrc = src: pinned.nix-gitignore.gitignoreSource [] src;
