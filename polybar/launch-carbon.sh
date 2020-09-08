7cb2e5741078fcd90f68f3dc451b390f6b4a3d60
Sam Doshi <sam@metal-fish.co.uk>
2020-09-08 18:48:48 +0100

    new carbon polybar setup

--- polybar/launch-carbon.sh
+++ polybar/launch-carbon.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 
-MONITOR_COUNT=$(xrandr | awk '/ connected/ && /[[:digit:]]x[[:digit:]].*+/{print $1}' | wc -l)
-
 CONFIG_FILE="$HOME/Linux/xmonad/polybar/config"
-
 killall -qu polybar
-
-if ((MONITOR_COUNT <= 1)); then
-    echo One
-    polybar --config=$CONFIG_FILE one &
-else
-    echo Multiple
-    polybar --config=$CONFIG_FILE left &
-    polybar --config=$CONFIG_FILE right &
-fi;
+polybar --config=$CONFIG_FILE desktop &
